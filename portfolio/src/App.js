2c45b04eddd00a61110e083d98ef51c6efa8b6d7
Gilan <gilserrant5@gmail.com>
2023-02-10 17:22:59 -0500

    nav started

---
+++ portfolio/src/App.js
@@ -0,0 +1,16 @@
+import NavBar from './scenes/Navbar';
+import { useState } from 'react';
+import useMediaQuery from './hooks/useMediaQuery';
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery('min-width: 1060px');
+
+  return (
+    <div className="app bg-deep-blue">
+      <NavBar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
+}
+
+export default App;
